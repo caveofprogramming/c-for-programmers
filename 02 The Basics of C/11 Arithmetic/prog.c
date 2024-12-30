49c1be1000364d329acc60c7244a8f2fce0042b5
John <john@Johns-iMac.station>
2024-12-30 06:06:47 +0100

    Updated.

---
+++ 02 The Basics of C/11 Arithmetic/prog.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main() 
+{
+    // +, -, *, /, %
+
+    printf("%d\n", 11%3);
+    printf("%f\n", (double)11/3);
+
+    int value1 = 3;
+    char value2 = 2;
+    printf("%d\n", value1 + value2);
+
+    return 0;
+}
