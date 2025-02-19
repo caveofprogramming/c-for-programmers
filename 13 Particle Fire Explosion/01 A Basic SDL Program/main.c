8c09dcdc9711d81b4c5146966f6101df78f86ff2
John <john@Johns-iMac.station>
2025-02-19 17:34:00 +0100

    Updated.

--- 13 Particle Fire Explosion/01 A Basic SDL Program/main.c
+++ 13 Particle Fire Explosion/01 A Basic SDL Program/main.c
@@ -5,6 +5,31 @@
 #include <string.h>
 #include <stdlib.h>
 
+/*
+ * Present a window filled with green.asm
+ *
+ * Next: refactor this into three functions and a struct.
+ * 
+ * The struct contains all variables that would otherwise
+ * have to be global, including the pixel buffer.
+ * It should also store the width and height of the window.
+ * 
+ * An initialization function creates this struct and returns 
+ * it after initializing the SDL graphics.asm
+ * 
+ * This is paired with a destroy function that frees all
+ * objects and quits the program.
+ * 
+ * In-between, an event loop function checks for events
+ * and draws whatever is currently in the pixel buffer.
+ * 
+ * main() should only call these three functions.
+ * 
+ * Consider prefixing the functions with a common prefix
+ * to ensure they don't conflict with any other function.
+ * E.g. gs_
+ */
+
 int main(int argc, char** argv)
 {
     /*
@@ -45,7 +70,7 @@ int main(int argc, char** argv)
 	}
 
     /*
-     * Draw on the screen.
+     * Buffer for drawing pixels.
      */
     uint32_t *buffer = malloc(width*height*4);
     
@@ -83,12 +108,15 @@ int main(int argc, char** argv)
             }
         }
 
+        /*
+         * Draw on the screen.
+         */
         if(SDL_UpdateTexture(texture, 
             NULL, 
             buffer, 
             width * 4) == false)
         {
-            printf("%s\n", SDL_GetError());
+            printf("Error updating texture: %s\n", SDL_GetError());
             return 0;
         }
     
